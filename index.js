2fdf7a876c1bb011187b35bc9471b49ea6cf1db5
Raul Sposito <raulsposito@gmail.com>
2023-02-28 03:35:16 -0300

    initial style

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'image-generator')));
+// set static folder for frontend
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
