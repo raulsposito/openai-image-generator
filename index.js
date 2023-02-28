fcfa9498aa73cb1895dfa6c3f8f9ad6d5fd551ee
Raul Sposito <raulsposito@gmail.com>
2023-02-28 02:02:55 -0300

    add path

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+const path = require("path");
 const express = require("express");
 const port = process.env.PORT || 8080;
-
 const dotenv = require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'image-generator')));
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
